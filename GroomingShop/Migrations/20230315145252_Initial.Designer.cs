2ff6c5e458688497408a962b5ec27deebd5aff53
Mike Donovan <mike@mikedonovan.dev>
2023-03-15 09:04:51 -0700

    Adjust style

--- GroomingShop/Migrations/20230315145252_Initial.Designer.cs
+++ GroomingShop/Migrations/20230315145252_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroomingShop.Migrations
 {
     [DbContext(typeof(GroomingShopContext))]
-    [Migration("20230315053956_Initial")]
+    [Migration("20230315145252_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
